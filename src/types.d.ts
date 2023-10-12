63f14b04f3b6e62c268a520c9e7520a7e1e46b76
Camil0Guerrero <guerrerocontrerascristiancamil@gmail.com>
2023-10-12 11:10:31 -0500

    Add types for CSS variables and user preferences

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,33 @@
-export type priorities = 'low' | 'medium' | 'high'
+import { COLORS, CONTENT, PRIORITIES, PROPERTIES_COLORS } from './const'
+
+export type Priorities = (typeof PRIORITIES)[keyof typeof PRIORITIES]
 
 export type APIToDoResponse = {
 	description: string
 	dueDate: Date | string
 	id?: number
-	priority: priorities
+	priority: Priorities
 	title: string
 }
 
 export type FiltersType = {
 	completed: boolean
 	dueDate: Date | string
-	priority: priorities | 'all'
+	priority: Priorities | 'all'
 	title: string
 	search: string
 }
+
+export type Color = `#${string}`
+
+type MainVariables = keyof typeof PROPERTIES_COLORS
+
+type PropertiesColors = keyof typeof COLORS
+
+export type VariablesCSS = MainVariables | PropertiesColors
+
+export type ColorsType = {
+	[key in VariablesCSS]: Color
+}
+
+export type Languages = keyof typeof CONTENT
